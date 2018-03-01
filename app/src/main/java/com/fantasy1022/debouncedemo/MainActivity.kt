643a0d9137ea261a82fe9209d2b0caa3e9c333ce
fantasy1022 <fantasy-1022@hotmail.com>
2018-03-01 23:12:05 +0800

    Add normal click case.

--- app/src/main/java/com/fantasy1022/debouncedemo/MainActivity.kt
+++ app/src/main/java/com/fantasy1022/debouncedemo/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.fantasy1022.debouncedemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var times: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        normalNextBtn.setOnClickListener {
+            playTimesTxt.setText(times++.toString())
+        }
+
     }
 }
