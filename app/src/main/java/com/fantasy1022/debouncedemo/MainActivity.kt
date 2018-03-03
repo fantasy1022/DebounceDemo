1cb089ab2a363fe08416d2e76c35f2b47daaa4ef
fantasy1022 <fantasy-1022@hotmail.com>
2018-03-03 22:21:03 +0800

    Add Rxview library and API.

--- app/src/main/java/com/fantasy1022/debouncedemo/MainActivity.kt
+++ app/src/main/java/com/fantasy1022/debouncedemo/MainActivity.kt
@@ -3,6 +3,10 @@ package com.fantasy1022.debouncedemo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import io.reactivex.android.schedulers.AndroidSchedulers
+import com.jakewharton.rxbinding2.view.RxView
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +19,15 @@ class MainActivity : AppCompatActivity() {
             playTimesTxt.setText(times++.toString())
         }
 
+        //throttleFirst
+        //throttleLast
+        //debounce
+        //sample
+        RxView.clicks(rxViewNextButton)
+                .throttleLast(3000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {  playTimesTxt.setText(times++.toString()) }
+
+        
     }
 }
